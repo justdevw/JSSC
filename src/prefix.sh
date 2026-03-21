b2c3897d3d3188c49c694c2bbbdf350f6bbefd6b
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-03-21 19:05:57 +0500

    Updates

--- src/prefix.sh
+++ src/prefix.sh
@@ -1,5 +1,5 @@
 for file in dist/jssc.js dist/jssc.cjs dist/jssc.mjs dist/jssc.min.js dist/worker.js dist/worker.min.js dist/cli.js; do
-    printf "/*\n\n%s\n\n*/\n\n/*! JSSC <https://jssc.js.org/> (c) 2025-2026 JustDeveloper <https://justdeveloper.is-a.dev/> */\n\n" "$(cat src/prefix.txt)" | cat - "$file" > temp.js && mv temp.js "$file"
+    printf "/*\n\n%s\n\n*/\n\n/*! JSSC <https://jssc.js.org/> (c) 2025-2026 JustDeveloper <https://justdeveloper.is-a.dev/> */\n\n" "$(cat LICENSE; printf "\n\n\n"; cat src/prefix.txt)" | cat - "$file" > temp.js && mv temp.js "$file"
 done
 
 for file in dist/cli.js; do
